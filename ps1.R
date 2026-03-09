92c7ecf17ab723ac47376fc399822a863cd349e9
pszewi <jakub.przewoski@gmail.com>
2026-03-10 00:34:04 +0100

    finishes Exe.2, cleans up code.

--- ps1.R
+++ ps1.R
@@ -40,9 +40,11 @@ library(RCT)
 # ---------------
 # funcs
 # ---------------
-diff_mean <- function(x, treatment_d) {
-    treated <- x[treatment_d == 1]
-    untreated <- x[treatment_d == 0]
+
+# function that calculates the difference in means between groups
+diff_mean <- function(data, treatment_d) {
+    treated <- filter(data, {{treatment_d}}==1)
+    untreated <- filter(data, {{treatment_d}}==0)
     
     test <- t.test(treated, untreated, alternative = "two.sided", var.equal = FALSE)
     
@@ -55,80 +57,82 @@ diff_mean <- function(x, treatment_d) {
         std_err = rep(std_err, 2)
     )
 
-    colnames(rtrn) <- c(paste0(colnames(x),"_mean_diff"), 
-                        paste0(colnames(x),"_std_err"))
+    colnames(rtrn) <- c(paste0(colnames(data),"_mean_diff"), 
+                        paste0(colnames(data),"_std_err"))
     rownames(rtrn) <- NULL
 
     rtrn
 }
 
+# function that creates the table
+make_table <- function(df, treat_col, x_vect){
+	initial_table <- df |> 
+	  select({{treat_col}}, x_vect) |>
+	  group_by({{treat_col}}) |>
+	  summarise(across(everything(), list(mean = mean, sd = sd)))
+	
+	# calculate the differences in mean over a loop
+	results <- list()
+	for (col in x_vect) {
+			# pass the input to another func 
+	    results[[col]] <- diff_mean(df[col], select({{df}}, {{treat_col}}))
+	}
+	results <- as.data.frame(do.call(cbind, results))
+	
+	TABLE.1 <- cbind(initial_table, results)
 
+	# pivot table for desired format	
+	TABLE.1_pivoted <- TABLE.1 |>
+	    pivot_longer(cols = !{{treat_col}}, names_to = "temp", values_to = "value") |>
+	    separate(temp, into = c("variable", "stat"), sep = "_(?=mean|sd|std_err)") |>
+	    mutate(variable = gsub('\\..*', '', variable)) |>
+	    pivot_wider(names_from = stat, values_from = value) |>
+	    pivot_wider(names_from = {{treat_col}}, values_from = c(mean, sd))
+
+	TABLE.1_pivoted
+}
 
 # --------------------
 # ------EXE 1---------
 # --------------------
 
 df <- read.csv("files/jtrain2.csv", sep = ";")
-# View(df)
 
-# subsetting for columns
+# subset for desired columns, make TABLE.1
 cols <- c("age", "educ", "black", "hisp", "nodegree", "re74", "re75")
-TABLE.1 <- df %>%
-  select(train, all_of(cols)) %>%
-  group_by(train) %>%
-  summarise(across(everything(), list(mean = mean, sd = sd)))
+TABLE.1 <- make_table(df, train, cols)
 
-# calculate the differences in mean
-results <- list()
-for (col in cols) {
-    results[[col]] <- diff_mean(df[col], df['train'])
-}
-results <- as.data.frame(do.call(cbind, results))
-
-TABLE.1 <- cbind(TABLE.1, results)
-
-# view(TABLE.1_p)
-
-TABLE.1_p <- TABLE.1 %>%
-    pivot_longer(cols = !train, names_to = "temp", values_to = "value") %>%
-    separate(temp, into = c("variable", "stat"), sep = "_(?=mean|sd|std_err)") %>%
-    mutate(variable = gsub('\\..*', '', variable)) %>%
-    pivot_wider(names_from = stat, values_from = value) %>%
-    pivot_wider(names_from = train, values_from = c(mean, sd))
-
-
-# b)
+# b)---------------------------------
 
 reg1 <- lm("re78 ~ train", df)
 
-# c)
+# c)---------------------------------
 forms = c("train", "train + age + educ + black + hisp",
           "train + age + educ + black + hisp + re74 + re75")
 
-models = list()
+models.1 = list()
 for (i in seq_along(forms)){
     reg = lm(paste0("re78 ~ ", forms[i]), df)
-    # print(summary(reg))
-    models[[i]] = reg
+    models.1[[i]] = reg
 }
 
-stargazer(
-    models, type='text'
+TABLE.2 <- stargazer(
+    models.1, type='text'
 )
 
-# d)
+# d)---------------------------------
 
 influence_train <- data.frame(train = (dfbeta(models[[3]])[, 2]))
 
 # get the 3,5,10th lowest obs
-smallest_b <- influence_train %>%
-  arrange(train) %>%
-  head(10) %>%
+smallest_b <- influence_train |>
+  arrange(train) |>
+  head(10) |>
   slice(c(3,5,10))
 
-biggest_b <- influence_train %>%
-  arrange(desc(train)) %>%
-  head(10) %>%
+biggest_b <- influence_train |>
+  arrange(desc(train)) |>
+  head(10) |>
   slice(c(3,5,10))
 
 df_restricted <- df[-c(as.numeric(rownames(smallest_b)), as.numeric(rownames(biggest_b))),]
@@ -145,36 +149,19 @@ stargazer(reg2, type='text')
 df.2 <- read.csv("files/jtrain3.csv", sep = ";")
 # View(df)
 
-# a)
+# a)---------------------------------
 
 # subsetting for columns
-cols <- c("age", "educ", "black", "hisp", "re74", "re75")
-TABLE.2 <- df.2 %>%
-  select(train, all_of(cols)) %>%
-  group_by(train) %>%
-  summarise(across(everything(), list(mean = mean, sd = sd)))
+cols.2 <- c("age", "educ", "black", "hisp", "re74", "re75")
 
-# calculate the differences in mean
-results <- list()
-for (col in cols) {
-    results[[col]] <- diff_mean(df.2[col], df['train'])
-}
-results <- as.data.frame(do.call(cbind, results))
+# make an intermediate table
+TABLE.1.2 <- make_table(df.2, train, cols.2)
+TABLE.1.2 <- TABLE.1.2 |>
+	mutate(variable = (paste0(variable, "_2")))
 
-TABLE.2 <- cbind(TABLE.2, results)
+TABLE.1 <- rbind(TABLE.1, TABLE.1.2)
 
-
-TABLE.2_p <- TABLE.2 %>%
-    pivot_longer(cols = !train, names_to = "temp", values_to = "value") %>%
-    separate(temp, into = c("variable", "stat"), sep = "_(?=mean|sd|std_err)") %>%
-    mutate(variable = gsub('\\..*', '', variable)) %>%
-    pivot_wider(names_from = stat, values_from = value) %>%
-    pivot_wider(names_from = train, values_from = c(mean, sd))
-
-
-TABLE_combined <- rbind(TABLE.1_p, TABLE.2_p)
-
-# b)
+# b)---------------------------------
 
 set.seed(12345)
 
@@ -184,20 +171,65 @@ random_alloc <- function(df, col_name){
 	df_sorted  <- df |>
 						arrange(random_vals)
 	df_sorted[col_name] <- c(rep_len(1, (nrow(df)/2)),rep_len(0, (nrow(df)/2)), 0)
-	df_sorted[-'random_vals']
+	df_sorted
 }
 
 
-df.2 <- random_alloc(df.2, 'treatment')
+df.2 <- random_alloc(df.2,'treated')
 
-# c)
-# TODO write down this func properly so it works 
-#df.2 <- treatment_assign(df.2, share_control = 0.5, n_t=1, key='random_vals')
+# c)---------------------------------
+assignments <- treatment_assign(df.2, share_control = 0.5,
+					strata_varlist = c("Q1"),
+					missfits = "global",
+					n_t=1,
+					key='random_vals')
 
-# d) 
-# cor.test(df.2$treatment, df.2$treated_2)
+df.2 <- left_join(df.2, assignments$data, by='random_vals')
+df.2 <- df.2 |> 
+	select(!c(strata, missfit)) |>
+	rename(treated_2= "treat")
+print(cor.test(df.2$treated, df.2$treated_2))
+
+# d) ---------------------------------
+TABLE.1.3 <- make_table(df.2, treated, cols.2)
+TABLE.1.3 <- TABLE.1.3 |>
+	mutate(variable = (paste0(variable, "_3")))
+
+TABLE.1 <- rbind(TABLE.1, TABLE.1.3)
+
+# TODO: write the comments!!
+
+# e) ---------------------------------
+forms = c("treated", "treated + age + educ + black + hisp",
+          "treated + age + educ + black + hisp + re74 + re75")
+
+models.2 = list()
+for (i in seq_along(forms)){
+    reg = lm(paste0("re78 ~ ", forms[i]), df.2)
+    # print(summary(reg))
+    models.2[[i]] = reg
+}
+
+TABLE.2 = stargazer(
+    models.1, models.2, type='text'
+)
+
+# TODO COMMENT ON FINDINGS!
+
+# f) ---------------------------------
+forms = c("train", "train + age + educ + black + hisp",
+          "train + age + educ + black + hisp + re74 + re75")
 
 
+models.3 = list()
+for (i in seq_along(forms)){
+    reg = lm(paste0("re78 ~ ", forms[i]), df.2)
+    # print(summary(reg))
+    models.3[[i]] = reg
+}
 
-
+TABLE.2 = stargazer(
+    models.1, models.2, models.3, type='text'
+)
+# TODO COMMENT ON FINDINGS!
 
