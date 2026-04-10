d59823f82b095eb9b513e970986dfaa1317dd3cb
madeliefvw <madeliefvanweerdenburg@hotmail.com>
2026-04-10 17:23:16 +0200

    1a

--- PS2V1.R
+++ PS2V1.R
@@ -28,3 +28,25 @@ library(bacondecomp)
 # library(did)        # Uncomment if using Callaway & Sant’Anna's estimator
 library(modelsummary) # For regression tables
 library(TwoWayFEWeights) # If implementing TWFE weights
+
+#excercise 1
+# -------- a --------
+#Given that divorce rates are
+#an average computed in each state and the variable stpop provides the population in
+#each of these states, which is the weight you should use when reporting the evolution
+#of divorce rates or a regression of divorce rates on unilateral divorce laws to 
+#match the analysis in Wolfers (2006)?
+
+#Wolfers uses the metric of number of divorces per 1,000 people per each US state 
+#for the div_rate variable. This creates a general metric with which you can properly 
+#compare between states, but when one wants to report the evolution of divorce rates 
+#or to run a regression of divorce rates on unilateral divorce laws, it is necessary to 
+#give each state the proper weight in order to not over account for smaller states and 
+#under account for larger states. (not having a bias toward smaller states).
+#The weight type we use for this is Analytic Weights (aweight / cellsize) as it 
+#corrects for cell means with different group sizes (stpop). Furthermore, it corrects
+#for the heteroskedasticity in the standard deviations, which would naturally
+#occur due to to different group sizes (here, the state populations).
+
+
+
